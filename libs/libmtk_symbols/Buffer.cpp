fdc15f5720b6f44c5835a0ec2d6ff3753b88a503
fire855 <thefire855@gmail.com>
2017-12-11 17:56:47 +0100

    shim: GraphicBufferConsumer shim: deprecated missing symbols for cam

--- libs/libmtk_symbols/Buffer.cpp
+++ libs/libmtk_symbols/Buffer.cpp
@@ -75,3 +75,20 @@ int _ZNK7android11MediaBuffer8refcountEv(android::MediaBuffer *self)
 #ifdef __cplusplus
 }
 #endif
+
+//
+extern "C" {
+#ifdef __LP64__
+    void _ZN7android18BufferItemConsumerC2ERKNS_2spINS_22IGraphicBufferConsumerEEEmib(const android::sp<android::IGraphicBufferConsumer>&, uint64_t, int, bool);
+
+    void _ZN7android18BufferItemConsumerC1ERKNS_2spINS_22IGraphicBufferConsumerEEEjib(const android::sp<android::IGraphicBufferConsumer>& consumer, uint32_t consumerUsage, int bufferCount, bool controlledByApp) {
+        _ZN7android18BufferItemConsumerC2ERKNS_2spINS_22IGraphicBufferConsumerEEEmib(consumer, static_cast<uint64_t>(consumerUsage), bufferCount, controlledByApp);
+    }
+#else
+    void _ZN7android18BufferItemConsumerC2ERKNS_2spINS_22IGraphicBufferConsumerEEEyib(const android::sp<android::IGraphicBufferConsumer>&, uint64_t, int, bool);
+
+    void _ZN7android18BufferItemConsumerC1ERKNS_2spINS_22IGraphicBufferConsumerEEEjib(const android::sp<android::IGraphicBufferConsumer>& consumer, uint32_t consumerUsage, int bufferCount, bool controlledByApp) {
+        _ZN7android18BufferItemConsumerC2ERKNS_2spINS_22IGraphicBufferConsumerEEEyib(consumer, static_cast<uint64_t>(consumerUsage), bufferCount, controlledByApp);
+    }
+#endif
+}

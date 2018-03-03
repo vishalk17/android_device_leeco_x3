2836d3c036d08b8eecab1e9dd0fd116608f07ed1
Sahil Soni <sscsps@gmail.com>
2018-03-03 18:57:27 +0530

    x3: Add audio_effects in xml format to device tree.
    Earlier we moved it to vendor tree, but since we might change it later,
    and its not the same as stock from device, we should keep it here.
    also, its originally present in .conf format, which is deprecated with android 7, so we convert it in .xml
    format.
    
    Signed-off-by: Sahil Soni <SscSPs@gmail.com>

--- device.mk
+++ device.mk
@@ -182,6 +182,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
 	$(DEVICE_PATH)/configs/audio/audio_device.xml:system/etc/audio_device.xml \
+	$(DEVICE_PATH)/configs/audio/audio_effects.xml:system/etc/audio_effects.xml \
 	$(DEVICE_PATH)/configs/audio/audio_em.xml:system/etc/audio_em.xml \
 	$(DEVICE_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf
 

abda79e9df18301d71b8895fb1cf3adb3e7a67e0
Sahil Soni <sscsps@gmail.com>
2018-03-04 17:58:36 +0530

    x3: Update audio_policy.conf to XML format
    May be this is a little more than needed, but whatever.
    
    Signed-off-by: Sahil Soni <SscSPs@gmail.com>

--- device.mk
+++ device.mk
@@ -180,11 +180,21 @@ PRODUCT_COPY_FILES += \
 	$(DEVICE_PATH)/configs/media/media_profiles.xml:system/etc/media_profiles.xml \
 	$(DEVICE_PATH)/configs/mtk_clear_motion.cfg:system/etc/mtk_clear_motion.cfg
 
+# Audio
+USE_XML_AUDIO_POLICY_CONF := 1
 PRODUCT_COPY_FILES += \
 	$(DEVICE_PATH)/configs/audio/audio_device.xml:system/etc/audio_device.xml \
 	$(DEVICE_PATH)/configs/audio/audio_effects.xml:system/etc/audio_effects.xml \
 	$(DEVICE_PATH)/configs/audio/audio_em.xml:system/etc/audio_em.xml \
-	$(DEVICE_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf
+	$(DEVICE_PATH)/configs/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
+	$(DEVICE_PATH)/configs/audio/a2dp_audio_policy_configuration.xml:/system/etc/a2dp_audio_policy_configuration.xml \
+	frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:/system/etc/audio_policy_volumes.xml \
+	frameworks/av/services/audiopolicy/config/default_volume_tables.xml:/system/etc/default_volume_tables.xml \
+	frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:/system/etc/r_submix_audio_policy_configuration.xml \
+	frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:/system/etc/usb_audio_policy_configuration.xml \
+	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+	frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+	frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml
 
 PRODUCT_COPY_FILES += \
 	$(DEVICE_PATH)/configs/permissions/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \

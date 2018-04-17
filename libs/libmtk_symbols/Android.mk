94d32fe16abf478ce6890fab9602781a521affdb
Sagar Shah <sagarshah2326@gmail.com>
2018-04-17 03:57:21 -0400

    libmtk_symbols: Add graphic buffer shim for camera
    conflict,
    
     cannot locate symbol "_ZN7android13GraphicBufferC1EjjijjP13native_handleb" referenced by "/system/lib/libcam_utils.so"...
    
    Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>
    Signed-off-by: vishalk17 <vishalkapadi95@gmail.com>

--- libs/libmtk_symbols/Android.mk
+++ libs/libmtk_symbols/Android.mk
@@ -23,7 +23,7 @@ LOCAL_SRC_FILES_64 += mtk_wvm64.cpp
 
 
 LOCAL_SHARED_LIBRARIES := libcutils libbinder libutils liblog libgui libui libicuuc libicui18n libcrypto libstagefright_foundation libssl libnativeloader libEGL  libGLESv2 libsync libbase libsensor libcam.utils.sensorlistener
-LOCAL_C_INCLUDES += frameworks/av/media/mtp/ system/core/include/ frameworks/rs/server/ frameworks/av/include/ hardware/libhardware/include/ frameworks/native/libs/sensor/include/
+LOCAL_C_INCLUDES += frameworks/av/media/mtp/ system/core/include/ frameworks/rs/server/ frameworks/av/include/ hardware/libhardware/include/ frameworks/native/libs/sensor/include/ frameworks/native/include
 LOCAL_MODULE := libmtk_symbols
 LOCAL_MODULE_TAGS := optional
 

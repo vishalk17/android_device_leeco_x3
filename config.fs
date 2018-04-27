2740b44ec006669898edfc9b377224ad27447720
vishalk17 <vishalkapadi95@gmail.com>
2018-04-27 04:36:23 +0000

    config.fs: refined

--- config.fs
+++ config.fs
@@ -1,5 +1,5 @@
 [vendor/bin/hw/android.hardware.bluetooth@1.0-service.mtk]
 mode: 0700
-user: bluetooth 
-group: bluetooth
-caps: CAP_SYS_NICE
+user: AID_BLUETOOTH 
+group: AID_BLUETOOTH
+caps: SYS_NICE

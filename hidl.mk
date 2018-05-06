5997cd7d4a5e3e71a857fe1e31b98e1680532c66
vishalk17 <vishalkapadi95@gmail.com>
2018-05-07 00:26:53 +0530

    hidl.mk: build few more hidl services

--- hidl.mk
+++ hidl.mk
@@ -8,7 +8,8 @@ PRODUCT_PACKAGES += \
 
 #GNSS HAL
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl
+    android.hardware.gnss@1.0-impl \
+    android.hardware.gnss@1.0-service
 
 # GPS force mode
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -27,6 +28,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.renderscript@1.0-impl
 
@@ -58,8 +60,8 @@ PRODUCT_PACKAGES += \
 
 # Keymaster HIDL
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl
-#    android.hardware.keymaster@3.0-service
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service
 
 # Gatekeeper HIDL
 PRODUCT_PACKAGES += \
@@ -83,7 +85,8 @@ PRODUCT_PACKAGES += \
 
 # Drm
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service
 
 # Health
 PRODUCT_PACKAGES += \

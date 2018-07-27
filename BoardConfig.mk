6623b296e483bcbde2b7ce14f1a0d4981077f56b
joshi3 <joshi.naga3@gmail.com>
2018-07-28 00:24:04 +0530

    add kernel headers
    Signed-off-by: joshi3 <joshi.naga3@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,6 +3,7 @@
 DEVICE_PATH := device/leeco/x3
 
 TARGET_SPECIFIC_HEADER_PATH := $(DEVICE_PATH)/include
+TARGET_BOARD_KERNEL_HEADERS := $(DEVICE_PATH)/kernel-headers
 
 MTK_K64_SUPPORT := yes
 USE_CAMERA_STUB := true

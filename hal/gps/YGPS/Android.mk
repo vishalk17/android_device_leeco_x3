539844500015655db5614ddb3a725c3db7a94986
vishalk17 <vishalkapadi95@gmail.com>
2018-09-19 03:50:08 +0000

    APP: YGPS: set LOCAL_PRIVATE_PLATFORM_APIS

--- hal/gps/YGPS/Android.mk
+++ hal/gps/YGPS/Android.mk
@@ -13,6 +13,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := YGPS
 LOCAL_CERTIFICATE := platform
 LOCAL_PROGUARD_ENABLED := disabled
+LOCAL_PRIVATE_PLATFORM_APIS := true
 
 include $(BUILD_PACKAGE)
 

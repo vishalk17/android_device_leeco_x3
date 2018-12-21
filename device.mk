36ba51070c1a28f076680c91d396804285fe79ec
vishalk17 <vishalkapadi95@gmail.com>
2018-12-21 14:13:25 +0000

    device.mk|configs: add _hals.conf for multihal_sensors

--- device.mk
+++ device.mk
@@ -283,6 +283,9 @@ PRODUCT_COPY_FILES += \
 	$(DEVICE_PATH)/configs/hostapd/hostapd.accept:system/vendor/etc/hostapd/hostapd.accept \
 	$(DEVICE_PATH)/configs/hostapd/hostapd.deny:system/vendor/etc/hostapd/hostapd.deny
 
+# MultiHal Sensors
+PRODUCT_COPY_FILES += \
+        $(DEVICE_PATH)/configs/sensors/_hals.conf:system/vendor/etc/sensors/_hals.conf
 
 # MTK Helpers
 PRODUCT_PACKAGES += \

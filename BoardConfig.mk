dfbc97b9479fb98602aa9c4846e15b9d4b6afd8f
Nicolas Geoffray <ngeoffray@google.com>
2019-02-22 15:38:22 +0000

    BoadConfig.mk:Generate the apex image
    Bug: 119800099
    Test: m
    Change-Id: I18a76766590db27a4c32534dc948f55f78da166c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -137,8 +137,9 @@ WIFI_DRIVER_STATE_CTRL_PARAM := "/dev/wmtWifi"
 WIFI_DRIVER_STATE_ON := 1
 WIFI_DRIVER_STATE_OFF := 0
 
-# Others
+# Others / Apex
 BUILD_BROKEN_DUP_RULES := true
+DEXPREOPT_GENERATE_APEX_IMAGE := true
 
 # Recovery
 ifeq ($(WITH_TWRP),true)

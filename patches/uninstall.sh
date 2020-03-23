d47874501d3acf353fee6d96f811c7f8841dca6c
vishalk17 <vishalkapadi95@gmail.com>
2020-03-23 09:56:24 +0000

    x3: update patches

--- patches/uninstall.sh
+++ patches/uninstall.sh
@@ -2,7 +2,7 @@ echo $1
 rootdirectory="$PWD"
 # ---------------------------------
 
-dirs="bionic frameworks/av frameworks/opt/telephony hardware/interfaces hardware/ril system/core"
+dirs="bionic frameworks/av frameworks/base  frameworks/opt/telephony hardware/interfaces hardware/ril system/core"
 
 for dir in $dirs ; do
 	cd $rootdirectory

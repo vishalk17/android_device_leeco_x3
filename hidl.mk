a673f89ea2ae208f752a3391f8c2bf11ff95a8c4
vishalk17 <vishalkapadi95@gmail.com>
2020-04-11 14:33:46 +0000

    manifest.xml: uprev health hidl 1.0 >> 2.0

--- hidl.mk
+++ hidl.mk
@@ -81,8 +81,8 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@1.0-impl \
-    android.hardware.health@1.0-service
+    android.hardware.health@2.0-impl \
+    android.hardware.health@2.0-service
 
 # Fingerprint
 PRODUCT_PACKAGES += \

ddb97908dec791ed5fa88b755ba86191350e1d10
vishalk17 <vishalkapadi95@gmail.com>
2020-06-22 07:55:55 +0000

    shim: callstack symbols
    - libutilscallstack.so has all callstack symbols
    
    - ref. https://android.googlesource.com/platform/system/core/+/master/libutils/CallStack.cpp
    
    conflicts ,
    
    It fixes _ZN7android9CallStackD1Ev  ~ android::CallStack::~CallStack() #libsrv_um.so
    
    - it fixes 06-10 17:34:33.269 447 447 E vndksupport: Could not load /vendor/lib/hw/camera.mt6795.so
    from default namespace: dlopen failed: cannot locate
      symbol "_ZN7android9CallStackC1Ev" referenced by "/system/vendor/lib/libcam_utils.so"....

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -129,6 +129,10 @@ TARGET_LD_SHIM_LIBS := \
     /system/vendor/lib64/egl/libGLESv2_mtk.so|libmtk_symbols.so \
     /system/vendor/lib/libBnMtkCodec.so|libmtk_symbols.so \
     /system/vendor/lib64/libBnMtkCodec.so|libmtk_symbols.so \
+    /system/vendor/lib/libcam_utils.so|libutilscallstack.so \
+    /system/vendor/lib64/libcam_utils.so|libutilscallstack.so \
+    /system/vendor/lib/libsrv_um.so|libutilscallstack.so \
+    /system/vendor/lib64/libsrv_um.so|libutilscallstack.so \
     /system/vendor/bin/guiext-server|libmtk_symbols.so \
     /system/vendor/xbin/mnld|libmtk_symbols.so
 

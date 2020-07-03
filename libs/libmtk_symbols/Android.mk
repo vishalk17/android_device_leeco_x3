466e7f9bc640305b093ab7546cb8b487b9d1bf1e
vishalk17 <vishalkapadi95@gmail.com>
2020-07-03 17:05:24 +0000

    for xlog symbol for audio

--- libs/libmtk_symbols/Android.mk
+++ libs/libmtk_symbols/Android.mk
@@ -3,8 +3,8 @@ LOCAL_PATH := $(call my-dir)
 
 # mtkc
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := libc.cpp
-LOCAL_SHARED_LIBRARIES := libc libutilscallstack
+LOCAL_SRC_FILES := xlog.c
+LOCAL_SHARED_LIBRARIES := libc
 LOCAL_MODULE := mtkc
 LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
